6be795dbe528918653500e9947eb2967dfe708a3
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-07-03 07:58:34 -0300

    Single Login logic

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from "./../../services/authentication.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
+  constructor(private authService: AuthenticationService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login() {
+    this.authService.login();
   }
-
 }
